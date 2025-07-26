ba27bc6c17e830fbb5c4460aee871351e14fe845
Manuel Werlberger <mwerlberger@users.noreply.github.com>
2025-07-26 12:47:22 +0200

    Config: allowunfree and stuff (#16)

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -1,6 +1,19 @@
 {
   description = "MW's Nix Configuration";
 
+  nixConfig = {
+    allowUnfree = true;
+    experimental-features = [ "nix-command" "flakes" ];
+    # # Optionally, keep your cross-platform/cachix settings:
+    # extra-platforms = [ "x86_64-linux" ];
+    # extra-substituters = [
+    #   "https://nix-community.cachix.org"
+    # ];
+    # extra-trusted-public-keys = [
+    #   "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    # ];
+  };
+
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
 
