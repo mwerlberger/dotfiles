51ec9c84483a071dac620390832acfae46a4a184
Manuel Werlberger <code@werlberger.org>
2025-09-27 15:41:30 +0200

    Backup old configs

--- nix-config/hosts/nixos/sagittarius/services/default.nix
+++ nix-config/hosts/nixos/sagittarius/services/default.nix
@@ -6,13 +6,10 @@
   imports = [
     ./ssh.nix
     ./samba.nix
-    # ./oidc.nix
     ./caddy.nix
     ./monitoring.nix
     ./immich.nix
     ./jellyfin.nix
-    # ./vpn.nix
-    # ./vpn-wg-mullvad.nix
     # ./sonarr.nix
     # ./radarr.nix
     # ./prowlarr.nix
