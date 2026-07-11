673bf254d7b1f6a8ca2d7470328982854fb433d1
Manuel Werlberger <code@werlberger.org>
2026-07-11 09:20:51 +0200

    Explicit version for postgres (16)

--- nix-config/hosts/nixos/sagittarius/services/postgresql.nix
+++ nix-config/hosts/nixos/sagittarius/services/postgresql.nix
@@ -4,6 +4,7 @@
   # PostgreSQL database server
   services.postgresql = {
     enable = true;
+    package = pkgs.postgresql_16;
 
     # Extensions required for Immich (VectorChord for vector search)
     extensions = with pkgs.postgresql16Packages; [
